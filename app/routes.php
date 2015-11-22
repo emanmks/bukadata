766b0c692ea46c7d20673fff73e600e585ef151c
Choz <vadz77@hotmail.com>
2015-11-22 17:25:04 +0800

    Fixed routes.php

--- app/routes.php
+++ app/routes.php
@@ -16,13 +16,8 @@ Route::get('/', function()
 	return View::make('hello');
 });
 
-<<<<<<< HEAD
-Route::resource('organization', 'OrganizationsController');
-Route::resource('uom', 'UomsController');
-Route::resource('transaction', 'TransactionsController');
-=======
 Route::resource('organizations', 'OrganizationsController');
+Route::resource('uoms', 'UomsController');
 Route::resource('transactions', 'TransactionsController');
->>>>>>> 0c2e42f312d18746c256506fa5c2038720884580
 Route::resource('people', 'PeoplesController');
 Route::resource('taxrates', 'TaxratesController');
