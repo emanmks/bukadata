98bc3789d44cd0d06137a5f2301892a8d6a57632
Achmad Fatoni <achmadfatony@gmail.com>
2015-12-06 10:13:34 +0700

    embed slideshare on homepage

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -6,7 +6,13 @@
         <section class="content">
 
             <h1>BukaData</h1>
-            
+
+            <h4>
+                BukaData menjawab kebutuhan akan adanya sebuah platform untuk merekam semua transaksi keuangan negara
+                dalam bentuk digital. Menjadi tantangan bagi instansi pemerintah maupun korporasi untuk membuktikan
+                komitmen mereka akan Kejujuran dan Keterbukaan yang akan menghasilkan integritas penyelenggara negara.
+            </h4>
+            <iframe style="border:none;width:750px;height:625px;" src="http://icant.co.uk/slidesharehtml/embed.php?url=http://www.slideshare.net/SolaimanMansyur/buka-data&width=700"></iframe>
         </section>
     </div>
 
